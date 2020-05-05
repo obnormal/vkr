57f5c78439f18dfa9c642fb30f7cfedc7cd0fd31
kamil <kamilgabdullin@gmail.com>
2020-05-05 06:59:27 +0300

    added host

--- untitled/settings.py
+++ untitled/settings.py
@@ -172,10 +172,10 @@ SITE_ID = 1
 
 ADMINS = [('admin','kamilgabdullin@gmail.com')]
 
-# if DEBUG:
-#     EMAIL_HOST = '127.0.0.1'
-#     EMAIL_PORT = 2525
-#     EMAIL_HOST_USER = ''
-#     EMAIL_HOST_PASSWORD = ''
-#     EMAIL_USE_TLS = False
-#     DEFAULT_FROM_EMAIL = 'testing@example.com'
+if DEBUG:
+    EMAIL_HOST = '127.0.0.1'
+    EMAIL_PORT = 2525
+    EMAIL_HOST_USER = ''
+    EMAIL_HOST_PASSWORD = ''
+    EMAIL_USE_TLS = False
+    DEFAULT_FROM_EMAIL = 'testing@example.com'
