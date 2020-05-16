49ed65675d6b78e4ee559322ebf9d6dfd30236c4
kamil <kamilgabdullin@gmail.com>
2020-05-16 18:08:07 +0300

    change static

--- untitled/settings.py
+++ untitled/settings.py
@@ -143,10 +143,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 AUTH_USER_MODELS = 'auth.User'
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
