985f809f010ceee3fb0f74d251e6f957e40e8838
kamil <kamilgabdullin@gmail.com>
2020-06-13 17:56:29 +0300

    try fix

--- untitled/settings.py
+++ untitled/settings.py
@@ -185,13 +185,13 @@ LOGGING = {
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 AUTH_USER_MODELS = 'auth.User'
